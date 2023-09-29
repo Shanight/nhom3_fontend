566d63f7347555a5f768578a8ebef285e4b3a762
shanight <phangiahuy8033@gmail.com>
2023-09-29 08:48:58 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Addt from './addt';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './header';
+import Login from './login';
+import Register from './register';
+import Add from './add';
+import Protected from './Proctected';
+import Chitiet from './chitiet';
+import Update from './update';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/add" element={<Protected Cmp={Add} />} />
+          <Route path="/addt" element={<Addt />} />
+          <Route path="/" element={<Chitiet />} />
+          <Route path="update/:id" element={<Protected Cmp={Update} />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
